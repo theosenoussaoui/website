d7a2a99074cc803d4ac73db7c9b87343f7cec2d9
Théo Senoussaoui <theosen95@gmail.com>
2023-04-24 20:29:18 +0200

    feat: adding pages for meetup, starting ui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,10 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+    },
+
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
