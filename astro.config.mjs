25f01662863de2cdadc276d7ca1dbcd1c396f7d8
Théo Senoussaoui <theosen95@gmail.com>
2023-09-08 17:17:05 +0200

    chore: upgrading and cleaning code for astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image(), preact()]
+  integrations: [tailwind(), mdx(), preact()],
 });
